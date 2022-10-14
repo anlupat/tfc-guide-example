b5f832be1d4ce8407bc58b287a5d558bf61b962d
Patrick Eshuis <patrick.eshuis@sogeti.com>
2022-10-14 14:44:29 +0200

    add cloud

--- versions.tf
+++ versions.tf
@@ -1,9 +1,16 @@
 terraform {
   required_providers {
     azurerm = {
-      source  = "hashicorp/azurerm"
+      source = "hashicorp/azurerm"
     }
 
+  }
+
+  cloud {
+    organization = "dwwm"
+    workspaces {
+      tags = ["dwwm"]
+    }
   }
 
   required_version = ">= 0.14.0"
