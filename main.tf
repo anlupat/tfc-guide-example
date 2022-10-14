f35d4393a3aae0e96f5fe92bd07b64e70812e00a
Patrick Eshuis <patrick.eshuis@sogeti.com>
2022-10-14 13:45:42 +0200

    addrg

--- main.tf
+++ main.tf
@@ -7,3 +7,8 @@ resource "azurerm_resource_group" "dwwm" {
   name     = "dwwm"
   location = var.region
 }
+
+resource "azurerm_resource_group" "dwwm2" {
+  name     = "dwwm2"
+  location = var.region
+}
