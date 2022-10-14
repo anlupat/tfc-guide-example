6d6bc3e437e864a75be96915feac808f11c9e715
Patrick Eshuis <patrick.eshuis@sogeti.com>
2022-10-14 13:35:26 +0200

    azurerm

--- main.tf
+++ main.tf
@@ -1,28 +1,10 @@
-provider "aws" {
+provider "azurerm" {
   region = var.region
+  features {
+  }
 }
 
-data "aws_ami" "ubuntu" {
-  most_recent = true
-
-  filter {
-    name   = "name"
-    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
-  }
-
-  filter {
-    name   = "virtualization-type"
-    values = ["hvm"]
-  }
-
-  owners = ["099720109477"] # Canonical
-}
-
-resource "aws_instance" "ubuntu" {
-  ami           = data.aws_ami.ubuntu.id
-  instance_type = var.instance_type
-
-  tags = {
-    Name = var.instance_name
-  }
+resource "azurerm_resource_group" "dwwm" {
+  name     = "dwwm"
+  location = var.region
 }
--- versions.tf
+++ versions.tf
@@ -1,14 +1,9 @@
 terraform {
   required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 3.28.0"
+    azurerm = {
+      source  = "hashicorp/azurerm"
     }
 
-    random = {
-      source  = "hashicorp/random"
-      version = "3.0.0"
-    }
   }
 
   required_version = ">= 0.14.0"
